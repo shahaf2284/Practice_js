37da7b919b104aaa759c4ba303086dd35d9aa06d
shahaf2284 <122786017+shahaf2284@users.noreply.github.com>
2024-04-24 18:36:53 +0300

    Update inheritance.js

--- inheritance.js
+++ inheritance.js
@@ -10,9 +10,32 @@ class Person
       console.log(this.name);
       console.log(this.id);
     }
-  
-  
-  
-  
   }
+
+class Student extends Person
+  {
+    constructor()
+    {
+        super();
+        this.faculty = "";
+    }
+    printData()
+    {
+        super.printData();
+        console.log(this.faculty);
+    }
+
+
+  }
+
+let p = new Person();
+p.name = "ron"
+p.id = 12
+
+let s = new Student();
+s.faculty = "Matematica"
+s.id=14
+s.name = "shahaf"
+s.printData();
+
 
